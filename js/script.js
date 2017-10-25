ed4c201f8096500afb410815f4bed8ce15870b10
Alexander Vinnikov <santystyle@mail.ru>
2017-10-25 19:12:18 +0300

    new theme

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.change-theme').on('click', function(event){
+    event.preventDefault();
+    $('.change-theme').fancybox();
+  });
+  $('.list-value').on('click', function(event){
+    event.preventDefault();
+    console.log('toogleClass');
+    $('#list-value').toggleClass('none');
+  })
+});
+
+$('.themes').on('change', function() {
+  var $current_theme = $(this).attr('value');
+  $('.link_stylesheet').attr('href', $current_theme);
+});
